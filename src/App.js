f2afa50ac5562e1e29153cd4e15bf2cd83950392
Aizhan Ai <76185271+aizhanai@users.noreply.github.com>
2021-06-21 23:19:18 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,6 @@
+import Index from "./index";
+
+function App() {
+  return <div className="App"></div>;
+}
+export default App;
---
+++ src/index.js
@@ -0,0 +1,68 @@
+import { useState } from "react";
+import ReactDOM from "react-dom";
+
+import "./styles.css";
+
+function App() {
+  const [data, setData] = useState("");
+  const calcBtns = [];
+  [9, 8, 7, 6, 5, 4, 3, 2, 1, 0, ",", "%"].forEach((item) => {
+    calcBtns.push(
+      <button
+        onClick={(e) => {
+          setData(data + e.target.value);
+        }}
+        value={item}
+        key={item}
+      >
+        {item}
+      </button>
+    );
+  });
+  return (
+    <div className="wrapper">
+      <div className="show-input">{data}</div>
+      <div className="digits flex">{calcBtns}</div>
+      <div className="modifiers subgrid">
+        <button onClick={() => setData(data.substr(0, data.length - 1))}>
+          {" "}
+          Clear{" "}
+        </button>
+        <button onClick={() => setData("")}>AC</button>
+      </div>
+      <div className="operrations subgrid">
+        <button onClick={(e) => setData(data + e.target.value)} value="+">
+          +
+        </button>
+        <button onClick={(e) => setData(data + e.target.value)} value="-">
+          -
+        </button>
+        <button onClick={(e) => setData(data + e.target.value)} value="*">
+          *
+        </button>
+        <button onClick={(e) => setData(data + e.target.value)} value="/">
+          /
+        </button>
+
+        <button
+          onClick={(e) => {
+            try {
+              setData(
+                String(eval(data)).length > 3 &&
+                  String(eval(data)).includes(".")
+                  ? String(eval(data).toFixed(4))
+                  : String(eval(data))
+              );
+            } catch (err) {
+              console.log(err);
+            }
+          }}
+          value="="
+        >
+          =
+        </button>
+      </div>
+    </div>
+  );
+}
+export default App;
